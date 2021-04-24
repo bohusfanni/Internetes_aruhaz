7423855f07f688f303c4e0cea8f24e231547e8e1
Kopitomi <you@example.com>
2021-04-24 17:22:32 +0200

    3 tabla kiiratas és felhasznalo helyes beszurasa DB be

--- code/oci.php
+++ code/oci.php
@@ -29,6 +29,54 @@ while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
 }
 print "</table>\n";
 
+$stid2 = oci_parse($conn, 'SELECT * FROM felhasznalo');
+if(!$stid2) {
+	$e = oci_error($conn);
+	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$r = oci_execute($stid2);
+if(!$r){
+	$e = oci_error($stid2);
+	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+
+print "<table border='1'>\n";
+while($row = oci_fetch_array($stid2, OCI_ASSOC+OCI_RETURN_NULLS)) {
+	print "<tr>\n";
+    foreach ($row as $item) {
+        print "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    print "</tr>\n";
+}
+print "</table>\n";
+
+oci_free_statement($stid2);
+
+$stid = oci_parse($conn, 'SELECT * FROM kategoria');
+if(!$stid) {
+	$e = oci_error($conn);
+	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$r = oci_execute($stid);
+if(!$r){
+	$e = oci_error($stid);
+	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+
+print "<table border='1'>\n";
+while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+	print "<tr>\n";
+    foreach ($row as $item) {
+        print "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    print "</tr>\n";
+}
+print "</table>\n";
 oci_free_statement($stid);
+
 oci_close($conn);
 ?>
