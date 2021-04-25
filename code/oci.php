08bf5ed9088069dfd3d0a15a28884f0a0ac9045d
Kopitomi <you@example.com>
2021-04-25 13:23:45 +0200

    +1 tábla +1 adat

--- code/oci.php
+++ code/oci.php
@@ -5,20 +5,18 @@ if (!$conn) {
 	$e = oci_error();
 	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
+
 print ("Elado tabla");
 $stid = oci_parse($conn, 'SELECT * FROM elado');
 if(!$stid) {
 	$e = oci_error($conn);
 	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
 $r = oci_execute($stid);
 if(!$r){
 	$e = oci_error($stid);
 	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
-
 print "<table border='1'>\n";
 while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
 	print "<tr>\n";
@@ -30,19 +28,18 @@ while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
 print "</table><br>\n";
 oci_free_statement($stid);
 
+
 print ("felhasznalo tabla");
 $stid2 = oci_parse($conn, 'SELECT * FROM felhasznalo');
 if(!$stid2) {
 	$e = oci_error($conn);
 	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
 $r = oci_execute($stid2);
 if(!$r){
 	$e = oci_error($stid2);
 	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
 print "<table border='1'>\n";
 while($row = oci_fetch_array($stid2, OCI_ASSOC+OCI_RETURN_NULLS)) {
 	print "<tr>\n";
@@ -52,8 +49,8 @@ while($row = oci_fetch_array($stid2, OCI_ASSOC+OCI_RETURN_NULLS)) {
     print "</tr>\n";
 }
 print "</table><br> \n";
-
 oci_free_statement($stid2);
+
 
 print ("Kategoria tabla");
 $stid = oci_parse($conn, 'SELECT * FROM kategoria');
@@ -61,14 +58,34 @@ if(!$stid) {
 	$e = oci_error($conn);
 	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
 $r = oci_execute($stid);
 if(!$r){
 	$e = oci_error($stid);
 	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
+print "<table border='1'>\n";
+while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+	print "<tr>\n";
+    foreach ($row as $item) {
+        print "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    print "</tr>\n";
+}
+print "</table><br>\n";
+oci_free_statement($stid);
 
 
+print ("Termék tabla");
+$stid = oci_parse($conn, 'SELECT * FROM termek');
+if(!$stid) {
+	$e = oci_error($conn);
+	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+$r = oci_execute($stid);
+if(!$r){
+	$e = oci_error($stid);
+	trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
 print "<table border='1'>\n";
 while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
 	print "<tr>\n";
