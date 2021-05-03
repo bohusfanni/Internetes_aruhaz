c0abb039986e076f959405cfa9743383d1e90906
kevekalotay <80063113+kevekalotay@users.noreply.github.com>
2021-05-03 22:36:53 +0200

    Add files via upload

---
+++ code/login.php
@@ -0,0 +1,49 @@
+<?php
+include "dbconnection.php";
+
+session_start ();
+$connection = DBconnection::getInstance();
+$statement = $connection->select(array("Jelszo"), array("Felhnev"), "Felhnev='".$_POST["uname"]."' AND Jelszo='".hash("MD5", $_POST["psw"])."'");
+$result = oci_execute($statement);
+$returned_rows = 0;
+$nev="";
+$row=array();
+
+while($row = oci_fetch_array($statement, OCI_ASSOC)){
+    $returned_rows++;
+    $nev=$row["NEV"];
+}
+if($returned_rows!=1){
+    echo "Sikertelen bejelentkezés";
+
+    elseif{
+    echo "Üdvözlöm ".$nev.", hamarosan átirányítom a fõoldalra";
+    session_start();
+    $_SESSION["fnev"]=$nev;
+    $statement = $connection->parseQuery("SELECT Felhanev FROM Rgazda WHERE Felhanev='".$nev."'");
+    $result = oci_execute($statement);
+    $count=0;
+    while(oci_fetch_array($statement, OCI_ASSOC)){
+        $count++;
+        echo "ADMIN!";
+    }
+    if($count==1){
+  $_SESSION["admin"]=true;
+    }
+    else{
+    echo "Üdvözlöm ".$nev.", hamarosan átirányítom a fõoldalra";
+    session_start();
+    $_SESSION["fnev"]=$nev;
+    $statement = $connection->parseQuery("SELECT felhasznnev FROM Elado WHERE felhasznev='".$nev."'");
+    $result = oci_execute($statement);
+    $count=0;
+    while(oci_fetch_array($statement, OCI_ASSOC)){
+        $count++;
+        echo "Elado!";
+    }
+    if($count==1){
+  $_SESSION["elado"]=true;
+    }
+die();
+
+?>
