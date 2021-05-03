d31e0c8aba610de183113417faa67f7b76f65936
Kopitomi <you@example.com>
2021-05-03 21:15:37 +0200

    main html

--- code/webshop_trigger.sql
+++ code/webshop_trigger.sql
@@ -1,10 +1,8 @@
-
-CREATE OR REPLACE TRIGGER megrendel
+create or replace TRIGGER megrendel
 AFTER INSERT OR UPDATE OR DELETE ON Rendel
 BEGIN
   UPDATE megrendeles
   SET megrendeles.osszeg = megrendeles.osszeg + (rendel.ar * rendel.darab)
   WHERE felhnev = megrendeles.felhnev;
 END;
-
-
+/
