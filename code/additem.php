12078f1f7d0e54554c83f20c2d4cb9d25cfa65a7
kevekalotay <keve.kalotay@gmail.com>
2021-05-04 22:27:19 +0200

    Termék plusz

--- code/additem.php
+++ code/additem.php
@@ -1,17 +1,37 @@
 <?php
+session_start();
+   include "dbconn.php";
+   $connection = DBconnection::getInstance();
+   
+   if(isset($_POST['itemid'])){ 
+       echo"az if jó";
+   $itemid = $_POST['itemid'];
+    $price = $_POST['price'];
+    $itemname = $_POST['itemname'];
+    $what = $_POST['what'];
+    $amount = $_POST['amount'];
+    $cat = $_POST['cat'];
+       
+   
+     
+    $query = "INSERT INTO TERMEK(TermekKod, Ar, nev, Leiras, DarabSzam, Kategoria) VALUES ($itemid, $price, $itemname, $what, $amount, $cat)";
+    
+    $res = oci_parse($connection->getConnection(), $query);
 
-include_once('dbconn.php')
-
-
-$itemid = $_POST['itemid'];
-$price = $_POST['price'];
-$itemname = $_POST['itemname'];
-$what = $_POST['what'];
-$amount = $_POST['amount'];
-$cat = $_POST['cat'];
-
-$add = 'INSERT INTO `TERMEK`(`TermekKod`, `Ar`, `nev`, `Leiras`, `DarabSzam`, `Kategoria`)
-                           VALUES ('$itemid', '$price', '$itemname', '$what', '$amount', '$cat')';
-
-mysqli_query($add);
+    oci_bind_by_name($res, ':itemid', $itemid);
+    oci_bind_by_name($res, ':price', $price);
+     oci_bind_by_name($res, ':itemname', $itemname);
+    oci_bind_by_name($res, ':what', $what);
+     oci_bind_by_name($res, ':amount', $amount);
+    oci_bind_by_name($res, ':cat', $cat);
+    
+    echo $itemid;
+    if(oci_execute($res)===false){
+        var_dump(oci_error($res));
+    }else{
+        //oci_commit($connection->getConnection());
+        echo "Sikeres felvitel";
+        header("Location: http://localhost/Internetes_aruhaz/code/add.php");
+    }
+}
 ?>
